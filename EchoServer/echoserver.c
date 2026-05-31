dd7de36b152417a6436e7e521b3022854b4a98d9
CoreThreads <leonnekamau28@gmail.com>
2026-06-01 00:35:28 +0300

    first project structure commit for the Echo Server

---
+++ EchoServer/echoserver.c
@@ -0,0 +1,28 @@
+#include "AsciiColors.h"
+#include "printBytes.h"
+#include <bits/sockaddr.h>
+#include <errno.h>
+#include <netinet/in.h>
+// #include <stdio.h>
+#include <stdio.h>
+#include <sys/socket.h>
+
+void EchoServer() {
+  int socketS, socketC;
+
+  socketS = socket(AF_INET, SOCK_STREAM, 0);
+  print_bytes(&socketS, sizeof(socketS));
+
+  struct sockaddr_in Structure_t;
+  Structure_t.sin_family = AF_INET;
+  Structure_t.sin_port = 8080;
+  Structure_t.sin_addr.s_addr = INADDR_ANY; // to take any ip 0.0.0.0
+
+  if (bind(socketS, (struct sockaddr *)&Structure_t, sizeof(Structure_t)) !=
+          -1 ||
+      errno) {
+    printf("%s Socket bind created successfully", AC_GREEN);
+  };
+}
+
+int main() { EchoServer(); }
---
+++ EchoServer/printBytes.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// Reusable function to print bytes with their total count
+void print_bytes(const void *ptr, size_t size) {
+  const unsigned char *byte_ptr = (const unsigned char *)ptr;
+
+  // Explicitly output the total byte count first
+  printf("(took %zu bytes): ", size);
+
+  for (size_t i = 0; i < size; i++) {
+    printf("%02x ", byte_ptr[i]);
+  }
+  printf("\n");
+}
