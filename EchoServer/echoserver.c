54c1b4e7e6ac4b8cb2a5af15af82eb084960b6ff
CoreThreads <leonnekamau28@gmail.com>
2026-06-10 16:38:51 +0300

    Just added accept to the echo server writing it from what we learnt

--- EchoServer/echoserver.c
+++ EchoServer/echoserver.c
@@ -1,11 +1,43 @@
 #include "AsciiColors.h"
 #include "printBytes.h"
 #include <bits/sockaddr.h>
-#include <errno.h>
 #include <netinet/in.h>
 // #include <stdio.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/socket.h>
+
+#include <dirent.h>
+#include <stdio.h>
+
+void print_my_fds() {
+  DIR *d = opendir("/proc/self/fd/");
+  if (!d)
+    return;
+
+  // Get the internal FD number used by opendir itself
+  int opendir_fd = dirfd(d);
+
+  struct dirent *dir;
+  int count = 0;
+
+  printf("\n--- Active File Descriptors ---\n");
+  while ((dir = readdir(d)) != NULL) {
+    if (dir->d_name[0] != '.') {
+      int current_fd = atoi(dir->d_name);
+
+      // Skip printing the temporary opendir file descriptor
+      if (current_fd == opendir_fd) {
+        continue;
+      }
+
+      printf("FD Number: %d\n", current_fd);
+      count++;
+    }
+  }
+  printf("Total Open FDs (Excluding opendir): %d\n", count);
+  closedir(d);
+}
 
 void EchoServer() {
   int socketS, socketC;
@@ -15,23 +47,42 @@ void EchoServer() {
 
   struct sockaddr_in Structure_t;
   Structure_t.sin_family = AF_INET;
-  Structure_t.sin_port = 8080;
+  Structure_t.sin_port = htons(8080);
   Structure_t.sin_addr.s_addr = INADDR_ANY; // to take any ip 0.0.0.0
+  socklen_t Structure_tLen = sizeof(Structure_t);
 
-  if (bind(socketS, (struct sockaddr *)&Structure_t, sizeof(Structure_t)) !=
-          -1 ||
-      errno) {
-    printf("%s Socket bind created successfully \n", AC_GREEN);
+  if (bind(socketS, (struct sockaddr *)&Structure_t, Structure_tLen) < 0) {
+
+    printf("%s [corethreads] Error Occurred \n", AC_RED);
 
   } else {
-    printf("%s Error Occurred \n", AC_RED);
+    printf("%s [corethreads] Socket bind created successfully \n", AC_GREEN);
   }
 
-  if (listen(socketS, 10) != -1 || errno) {
-    printf("%s Socket turned into a passive server\n", AC_GREEN);
+  if (listen(socketS, 10) < 0) {
+
+    printf("%s [corethreads] Error Occurred \n", AC_RED);
+
   } else {
-    printf("%s Error Occurred >-<\n", AC_RED);
+    printf("%s [corethreads] Socket turned into a passive server\n", AC_GREEN);
+  }
+
+  struct sockaddr_in Client_Structure;
+  socklen_t clientLen = sizeof(Client_Structure);
+
+  while (1) {
+    socketC = accept(socketS, (struct sockaddr *)&Client_Structure, &clientLen);
+    print_bytes(&socketC, sizeof(socketC));
+    if (socketC < 0) {
+      printf("%s [corethreads] Error Occurred\n", AC_RED);
+    } else {
+
+      printf("%s [corethreads] Connection Established[+].....\n ", AC_GREEN);
+    }
   }
 }
 
-int main() { EchoServer(); }
+int main() {
+  EchoServer();
+  print_my_fds();
+}
