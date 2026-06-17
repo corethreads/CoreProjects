7ce08c8e82fb2a0f50568aad8c7f00cbae95f801
CoreThreads <leonnekamau28@gmail.com>
2026-06-18 02:16:39 +0300

    Just read and write in my EchoServer rewrote them 6 times in order to clear and understand the errors

--- EchoServer/echoserver.c
+++ EchoServer/echoserver.c
@@ -1,12 +1,14 @@
 #include "AsciiColors.h"
 #include "printBytes.h"
 #include <bits/sockaddr.h>
+#include <errno.h>
 #include <netinet/in.h>
 // #include <stdio.h>
 #include <dirent.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
 
@@ -41,10 +43,11 @@ void print_my_fds() {
 
 void ErrorLog(char *message) {
 
-  printf("%s [ERROR] %s %s\n ", AC_RED, message, AC_WHITE);
+  printf("%s [ERROR] %s %s %s\n ", AC_RED, message, strerror(errno), AC_WHITE);
 }
 void SuccessLog(char *message) {
-  printf("%s [SUCCESS] %s %s\n", AC_GREEN, message, AC_WHITE);
+  printf("%s [SUCCESS] %s %s %s\n", AC_GREEN, message, strerror(errno),
+         AC_WHITE);
 }
 void EchoServer() {
   int socketS, socketC;
@@ -54,7 +57,7 @@ void EchoServer() {
 
   struct sockaddr_in Structure_t;
   Structure_t.sin_family = AF_INET;
-  Structure_t.sin_port = htons(8080);
+  Structure_t.sin_port = htons(4040);
   Structure_t.sin_addr.s_addr = INADDR_ANY; // to take any ip 0.0.0.0
   socklen_t Structure_tLen = sizeof(Structure_t);
 
@@ -88,15 +91,27 @@ void EchoServer() {
     }
 
     char MessageStore[1024] = {0};
-    ssize_t ReadFileDescriptor;
+    ssize_t ReadFileDescriptor, WriteDataFromFD;
     ReadFileDescriptor = read(socketC, MessageStore, 1023);
 
     if (ReadFileDescriptor < 0) {
       ErrorLog("Nothing Read in File Descriptor");
     } else {
       SuccessLog("File Descriptor read SuccessFully");
+      print_bytes(&ReadFileDescriptor, sizeof(ReadFileDescriptor));
+    }
+
+    WriteDataFromFD = write(socketC, MessageStore, ReadFileDescriptor);
+    if (WriteDataFromFD < 0) {
+      ErrorLog("Nothing to Write");
+    } else {
+      SuccessLog("Writing...");
+      print_bytes(&ReadFileDescriptor, sizeof(ReadFileDescriptor));
     }
   }
+
+  shutdown(socketC, SHUT_RDWR);
+  shutdown(socketS, SHUT_RDWR);
 }
 
 int main() {
